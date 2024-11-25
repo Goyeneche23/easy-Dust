52be294badff7827af2baeeb0ba824aee3260fc5
Goyeneche23 <85038931+Goyeneche23@users.noreply.github.com>
2024-11-25 11:18:57 -0600

    Add files via upload

---
+++ Interfaz/program.js
@@ -0,0 +1,54 @@
+// program.js
+
+const dust = document.getElementById("Dust");
+const estado = document.querySelector('.estado');
+const line = document.querySelector('.line');
+const circle = document.querySelector('.circle');
+
+function tempChange() {
+    if (estado.classList.contains('cold')) {
+        estado.classList.remove('cold');
+        estado.classList.add('hot');
+        line.classList.add('hot');
+        circle.classList.add('hot');
+    } else {
+        estado.classList.remove('hot');
+        estado.classList.add('cold');
+        line.classList.remove('hot');
+        circle.classList.remove('hot');
+    }
+}
+
+function acercaDe(){
+    window.location.href = "https://github.com/Goyeneche23/easy-Dust";
+}
+
+function racksList() {
+    const menuRacks = document.querySelector('.menuRacks');
+    menuRacks.style.display = menuRacks.style.display === 'none' ? 'block' : 'none';
+}
+
+function loggeado(){
+    window.location.href= "index.html";
+}
+
+function logIn(){
+    window.location.href = "log.html";
+}
+
+function refreshMenu(){
+    window.location.href = "";
+}
+
+function verifLog(){
+    
+}
+
+function showSnow() {
+    dust.style.display = "block";
+}  
+  // Ocultar nieve
+function hideSnow() {
+    dust.style.display = "none";
+}
+
